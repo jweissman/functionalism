c99d95242cb624908467d30c8e89a2603d5a310d
Joseph Weissman <joe@bignerdranch.com>
2016-01-29 11:50:21 -0500

    split at in terms of take/drop

--- lib/functionalism/split_at.rb
+++ lib/functionalism/split_at.rb
@@ -1,11 +1,8 @@
 module Functionalism
-  # TODO fold?
-  SplitAt = lambda do |n, arr|
-    return [[],arr] if n == 0
-    return [[],[]] if arr.empty?
-    raise "Can't split on negative index!" unless n > 0
-    x, xs = First[arr], Rest[arr]
-    xs_prime, xs_prime_prime = *SplitAt[n-1,xs]
-    [ Cons[xs_prime, x], xs_prime_prime ]
-  end.curry
+  SplitAt = lambda do |n|
+    raise "Can't split on negative index!" unless n >= 0
+    lambda do |arr|
+      [ Take[n,arr], Drop[n, arr] ]
+    end
+  end
 end
